b7a8e3f583deef9833c7702688729d380f10e3f0
刘金发 <liujinfa9242@dingtalk.com>
2023-12-30 03:46:05 +0800

    update local-dev-depoly

--- dev_utls/dev-container/local-dev-deploy/projects/deploy/deploy_to_docker.sh
+++ dev_utls/dev-container/local-dev-deploy/projects/deploy/deploy_to_docker.sh
@@ -10,17 +10,30 @@ current_basename=$(basename "$0")
 # 指定当前目录
 deploy_dir=$current_dir
 
+package=$1
 service_name=$1
+
+if [[ $package == "package" ]]; then
+    service_name=$2
+fi
 
 execute_sh_files() {
     top_directory=$(readlink -f "$1")
+    package="$2"
     service_name="$2"
+
+    if [[ $package != "package" ]]; then
+        return;
+    fi
+
+    service_name="$3"
+
     for file in "$top_directory"/*; do
         if [[ -d "$file" ]]; then
             if [[ -n "$service_name" && $(basename "$file") != "$service_name" ]]; then
                 continue;
             fi
-            execute_sh_files "$file" "$service_name"  # 递归调用，进入子目录
+            execute_sh_files "$file" "$package" "$service_name"  # 递归调用，进入子目录
         elif [[ -f "$file" && "$file" == *.sh && $(basename "$file") != $current_basename ]]; then
             echo "Executing $file basename: $(basename "$file") current_basename: $current_basename"
             chmod +x "$file"  # 添加执行权限
@@ -30,7 +43,7 @@ execute_sh_files() {
     done
 }
 
-execute_sh_files "$deploy_dir" "$service_name"
+execute_sh_files "$deploy_dir" "$package" "$service_name"
 
 cd $deploy_dir
 
